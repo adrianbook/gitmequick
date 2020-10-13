da96e8ddc86c88d638a54630d4b7b7e604243790
desktop-d7dqtnr\adria <adrboo2001@skola.goteborg.se>
2020-10-13 18:48:54 +0200

    changes to Gitme.java

--- Gitme.java
+++ Gitme.java
@@ -1,5 +1,7 @@
 class Gitme {
     public static void main(String[] args) {
-        System.out.println("SÅ SKOJ VI SKAHA HA DET MED GITT!");
+
+        System.out.println("SÅ SKOJ VI SKA HA DET MED GITT!");
+        System.out.println("SÅ MYCKET HÄRLIG KODD VI SKA DELA!");
     }
 }
